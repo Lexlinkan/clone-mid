94830ce98100ac5b583da3685ef488adaf0fb17f
Lexlinkan <alexander-lindqvist@live.se>
2023-12-20 16:12:06 +0100

    Create route progress

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -12,10 +12,14 @@ const CreatePost = () => {
     prompt: "",
     photo: "",
   });
-  const [generating, setGenerating] = useState(false);
+  const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = () => {}
+
+  const handleChange = (e) => {}
+
+  const handleSurpriseMe = () => {}
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -32,16 +36,47 @@ const CreatePost = () => {
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormField 
-            LabelName="Your name"
+            labelName="Your name"
             type="text"
             name="name"
             placeholder="John Doe"
-            
+            value={form.name}
+            handleChange={handleChange}
           /> 
+          <FormField 
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="panda mad scientist mixing sparkling chemicals, digital art"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          /> 
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img 
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img 
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
         </div>
 
+        
       </form>
-
     </section>
   );
 };
